838a2b061707acbcceac63fd44e2faaf983c8f63
Alex Stelzer <alst2225@colorado.edu>
2024-04-04 12:17:07 -0600

    Lab 11 stuff - /login test page isn't working

---
+++ ProjectSourceCode/test/server.spec.js
@@ -0,0 +1,46 @@
+// ********************** Initialize server **********************************
+
+const server = require('../src/index'); //TODO: Make sure the path to your index.js is correctly added
+
+// ********************** Import Libraries ***********************************
+
+const chai = require('chai'); // Chai HTTP provides an interface for live integration testing of the API's.
+const chaiHttp = require('chai-http');
+chai.should();
+chai.use(chaiHttp);
+const {assert, expect} = chai;
+
+// ********************** DEFAULT WELCOME TESTCASE ****************************
+
+describe('Server!', () => {
+  // Sample test case given to test / endpoint.
+  it('Returns the default welcome message', done => {
+    chai
+      .request(server)
+      .get('/welcome')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.status).to.equals('success');
+        assert.strictEqual(res.body.message, 'Welcome!');
+        done();
+      });
+  });
+});
+
+// *********************** TODO: WRITE 2 UNIT TESTCASES **************************
+
+describe('Testing ', () => {
+    it('positive : /login', done => {
+      chai
+        .request(server)
+        .post('/login')
+        .send({username: 'andrewsFootFungus', password: 'bigFella'})
+      .end((err, res) => {
+        res.should.have.status(302); // Expecting a redirect status code
+        res.should.redirect('/home'); // Expecting a redirect to /login with the mentioned Regex
+        done();
+      });
+    });
+});
+
+// ********************************************************************************
