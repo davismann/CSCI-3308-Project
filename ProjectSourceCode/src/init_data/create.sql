40db7eb40f099d45bb920ed895ceed2e25bf0701
Davis Mann <daviscmann@gmail.com>
2024-04-03 20:23:52 -0600

    General additions.

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -3,12 +3,13 @@ CREATE TABLE IF NOT EXISTS users (
   password CHAR(60) NOT NULL,
   height CHAR(3),
   weight VARCHAR(4),
+  age CHAR(3),
   activity_level VARCHAR(20),
   weight_goal VARCHAR(20)
 );
 
 CREATE TABLE IF NOT EXISTS tracker (
-    track_id INT SERIAL PRIMARY KEY,
+    track_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     date DATE NOT NULL,
     protein VARCHAR(5) NOT NULL,
