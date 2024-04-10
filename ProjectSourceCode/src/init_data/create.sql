8c25025e997e125efdb01abc60eddd2ff6454899
Alex Stelzer <alst2225@colorado.edu>
2024-04-10 12:21:44 -0600

    edited tracker table

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -13,12 +13,9 @@ CREATE TABLE IF NOT EXISTS tracker (
     track_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     date DATE NOT NULL,
-    protein VARCHAR(5) NOT NULL,
-    fats VARCHAR(5) NOT NULL,
+    meal_name VARCHAR(50) NOT NULL,
+    meal_cals VARCHAR(5) NOT NULL,
     total_cals VARCHAR(5) NOT NULL,
-    dietary_fibers VARCHAR(5) NOT NULL,
-    carbohydrates VARCHAR(5) NOT NULL,
     goal_calories VARCHAR(5) NOT NULL,
-
     FOREIGN KEY (username) REFERENCES users(username)
 );
